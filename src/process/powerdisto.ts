324d44fb388697bb99b287a87f8f66c50f480bcc
Aarni Koskela <akx@iki.fi>
2022-05-27 17:08:08 +0300

    Sign of the times

--- src/process/powerdisto.ts
+++ src/process/powerdisto.ts
@@ -3,8 +3,11 @@ import { applyFunctionToAllChannels } from "./utils";
 function distortMono(channelData: Float32Array, power: number) {
   const newData = new Float32Array(channelData);
   if (power < 0) power = 1 / -power;
+  else power = 1 / power;
   for (let i = 0; i < newData.length; i++) {
-    newData[i] = Math.pow(newData[i], power);
+    const sign = newData[i] < 0 ? -1 : 1;
+    const val = Math.pow(newData[i] * sign, power) * sign;
+    newData[i] = isNaN(val) ? 0 : val;
   }
   return newData;
 }
